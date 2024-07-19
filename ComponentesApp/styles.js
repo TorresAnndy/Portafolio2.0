07e6a16f5c7ce195fe7938f550f55fc207e52b55
TorresAnndy <andygonzales2003@gmail.com>
2024-07-19 10:45:54 -0500

    modified:   ComponentesApp/styles.js

--- ComponentesApp/styles.js
+++ ComponentesApp/styles.js
@@ -1,82 +1,85 @@
 // styles.js
-import { StyleSheet, Platform, Dimensions } from 'react-native';
+import { StyleSheet, Platform, Dimensions } from "react-native";
 
-const { height: screenHeight } = Dimensions.get('window');
+const { height: screenHeight } = Dimensions.get("window");
 const sectionPadding = 20;
 const sectionMarginBottom = 30;
 
 export const globalStyles = StyleSheet.create({
   pagina: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
+    paddingTop: Platform.OS === "android" ? 30 : 0,
   },
   barra: {
     flexGrow: 1,
     padding: 20,
   },
-  
   imagencircular: {
     width: 300,
     height: 300,
     borderRadius: 150, // Ajusta el radio para que sea la mitad del ancho/alto
-    position: 'absolute', // Para posicionamiento absoluto dentro del contenedor
-    top: '50%', // Centra verticalmente
-    left: '50%', // Centra horizontalmente
+    position: "absolute", // Para posicionamiento absoluto dentro del contenedor
+    top: "50%", // Centra verticalmente
+    left: "50%", // Centra horizontalmente
     transform: [
       { translateX: -700 }, // Mueve la imagen a la izquierda para centrarla
       { translateY: -300 }, // Mueve la imagen arriba para centrarla
     ],
   },
   Hola: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 50,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
-    color: '#000',
+    color: "#000",
   },
 
   titulo: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
-    color: '#000', 
+    color: "#000",
   },
 
   Junior: {
     fontSize: 30,
-    color: '#555',
+    color: "#555",
     marginBottom: 21,
-    justifyContent: 'center',
-    textAlign: 'center',
+    justifyContent: "center",
+    textAlign: "center",
   },
 
   subtitulos: {
     fontSize: 20,
-    color: '#555',
+    color: "#555",
     marginBottom: 21,
   },
 
   section: {
-    height: screenHeight - sectionPadding - sectionMarginBottom - (Platform.OS === 'android' ? 30 : 0),
+    height:
+      screenHeight -
+      sectionPadding -
+      sectionMarginBottom -
+      (Platform.OS === "android" ? 30 : 0),
     marginBottom: sectionMarginBottom,
     padding: sectionPadding,
     borderWidth: 2,
-    borderColor: '#007bff',
+    borderColor: "#007bff",
     borderRadius: 10,
-    textAlign: 'center',
-    justifyContent: 'center',
+    textAlign: "center",
+    justifyContent: "center",
   },
   sectionContent: {
-    alignItems: 'center',
-    textAlign: 'center',
+    alignItems: "center",
+    textAlign: "center",
     fontSize: 16,
     lineHeight: 22,
-    color: '#000', 
+    color: "#000",
   },
   project: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 20,
   },
   projectImage: {
@@ -104,13 +107,13 @@ export const globalStyles = StyleSheet.create({
     marginRight: 10,
   },
   botonesgithub: {
-    backgroundColor: '#375ECE',
+    backgroundColor: "#375ECE",
     marginBottom: 5,
     padding: 10,
     borderWidth: 2,
-    borderColor: '#000',
+    borderColor: "#000",
     borderRadius: 10,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   projectImgbdd: {
     width: 85,
@@ -124,64 +127,64 @@ export const globalStyles = StyleSheet.create({
   projectDescription: {
     fontSize: 14,
     marginBottom: 5,
-    color: '#000', 
+    color: "#000",
   },
   contactLinks: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     marginTop: 10,
   },
   contactLink: {
     marginHorizontal: 10,
     padding: 10,
     borderWidth: 1,
-    borderColor: '#007bff',
+    borderColor: "#007bff",
     borderRadius: 5,
   },
   contactLinkText: {
-    color: '#007bff',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    color: "#007bff",
+    fontWeight: "bold",
+    textAlign: "center",
   },
-  
+
   menuContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     right: 20,
     zIndex: 10,
   },
   menu: {
-    position: 'absolute',
+    position: "absolute",
     top: 55, // Ajusta la posición según sea necesario
     right: 30,
     padding: 10,
     borderRadius: 5,
-    borderColor: '', // Solo el borde que quieres
+    borderColor: "", // Solo el borde que quieres
     zIndex: 10,
   },
   buttonText: {
-    color: '#fff', // Color del texto
+    color: "#fff", // Color del texto
     fontSize: 16, // Tamaño del texto
-    fontWeight: 'bold', // Negrita
+    fontWeight: "bold", // Negrita
   },
   menuButton: {
     width: 40, // Ajusta el tamaño según sea necesario
     height: 40,
     borderRadius: 20, // Si no quieres un círculo, ajusta esto a 0
-    backgroundColor: '#ddd',
+    backgroundColor: "#ddd",
     margin: 10,
   },
   button: {
     margin: 10,
-    backgroundColor: '#007bff', // Color de fondo del botón
+    backgroundColor: "#007bff", // Color de fondo del botón
     paddingVertical: 15, // Espaciado vertical
     paddingHorizontal: 20, // Espaciado horizontal
     borderRadius: 10, // Bordes redondeados
     borderWidth: 1, // Ancho del borde
-    borderColor: '#0056b3', // Color del borde
-    alignItems: 'center', // Centra el texto horizontalmente
-    justifyContent: 'center', // Centra el texto verticalmente
-    shadowColor: '#000', // Color de la sombra
+    borderColor: "#0056b3", // Color del borde
+    alignItems: "center", // Centra el texto horizontalmente
+    justifyContent: "center", // Centra el texto verticalmente
+    shadowColor: "#000", // Color de la sombra
     shadowOffset: { width: 9, height: 10 }, // Desplazamiento de la sombra
     shadowOpacity: 8, // Opacidad de la sombra
     shadowRadius: 6, // Difuminado de la sombra
